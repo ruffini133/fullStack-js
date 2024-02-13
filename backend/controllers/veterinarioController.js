56e6ff5662b139d06aea25167e7bbcd33a499e4d
Ramon Cisneros <cisneroslopezramon1@gmail.com>
2024-02-12 23:18:50 -0600

    Refactor code for connecting to MongoDB and registering a new Veterinario

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -1,10 +1,28 @@
-const registrar = (req, res) => {
+import Veterinario from "../models/Veterinario.js";
+
+const registrar = async (req, res) => {
 	// Cuando se envia informacion a una api de express, se almacena en req.body
 
-	console.log(req.body);
-	const { email, password } = req.body;
+	const { email } = req.body;
 
-	res.json({ msg: "Registrando Usuario" });
+	// Prevenir usuarios duplicados
+	const existeUsuario = await Veterinario.findOne({ email });
+
+	if (existeUsuario) {
+		const error = new Error("El correo ya esta registrado");
+		return res.status(400).json({ msg: error.message });
+	}
+
+	try {
+		// Guardar un Nuevo Veterinario
+		const veterinario = new Veterinario(req.body);
+		// Usamos un await para esperar a que se guarde el veterinario y no se ejecuten las siguientes lineas de codigo
+		const veterinarioGuardado = await veterinario.save();
+
+		res.json(veterinarioGuardado);
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 const perfil = (req, res) => {
