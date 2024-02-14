897cd58f5c7a6b243eb61b89be4c1ba737bd295d
Ramon Cisneros <cisneroslopezramon1@gmail.com>
2024-02-14 10:11:39 -0600

    Refactor pacienteController.js to use ES6 import syntax and add async/await to agregarPaciente and obtenerPacientes functions

--- backend/controllers/pacienteController.js
+++ backend/controllers/pacienteController.js
@@ -1,4 +1,22 @@
-const agregarPaciente = (req, res) => {};
-const obtenerPacientes = (req, res) => {};
+import Paciente from "../models/Paciente.js";
+
+const agregarPaciente = async (req, res) => {
+	const paciente = new Paciente(req.body);
+	paciente.veterinario = req.veterinario._id;
+
+	try {
+		const pacienteAlmacenado = await paciente.save();
+		res.json(pacienteAlmacenado);
+	} catch (error) {
+		console.log(error);
+	}
+};
+const obtenerPacientes = async (req, res) => {
+	const pacientes = await Paciente.find()
+		.where("veterinario")
+		.equals(req.veterinario);
+
+	res.json(pacientes);
+};
 
 export { agregarPaciente, obtenerPacientes };
