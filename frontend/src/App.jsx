2d659357875eb6833890ee3be8a3ec17597e42d4
Ramon Cisneros <cisneroslopezramon1@gmail.com>
2024-02-14 21:03:02 -0600

    Add new files and update existing files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthLayout from "./layout/AuthLayout";
+import Login from "./paginas/Login";
+
+function App() {
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<AuthLayout />}>
+						<Route index element={<Login />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
+}
+
+export default App;
