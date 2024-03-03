9c5ffb4602f22c7ffb77bd45db86d567a739733f
Ramon Cisneros <cisneroslopezramon1@gmail.com>
2024-03-03 13:46:34 -0600

    Refactor AuthProvider and add useAuth hook

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -5,7 +5,16 @@ const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState({});
 
-  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider
+      value={{
+        auth,
+        setAuth,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export { AuthProvider };
