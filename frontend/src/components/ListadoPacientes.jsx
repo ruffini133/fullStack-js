40e23bf635b23a2169b3dd84fe18604bbe421d4a
Ramon Cisneros <cisneroslopezramon1@gmail.com>
2024-03-04 00:02:27 -0600

    Add dynamic rendering of patient list based on data availability

--- frontend/src/components/ListadoPacientes.jsx
+++ frontend/src/components/ListadoPacientes.jsx
@@ -1,7 +1,30 @@
+import usePacientes from "../hooks/usePacientes";
+
 const ListadoPacientes = () => {
+  const { pacientes } = usePacientes();
+  console.log(pacientes);
+
   return (
     <>
-      <h1>ListadoPacientes</h1>
+      {pacientes.length ? (
+        <>
+          <h2 className="text-center text-3xl font-black">Listado Pacientes</h2>
+          <p className="mb-10 mt-5 text-center text-xl">
+            Administra tus {""}
+            <span className="font-bold text-indigo-600">Pacientes y Citas</span>
+          </p>
+        </>
+      ) : (
+        <>
+          <h2 className="text-center text-3xl font-black">No Hay Pacientes</h2>
+          <p className="mb-10 mt-5 text-center text-xl">
+            Comienza agregando pacientes {""}
+            <span className="font-bold text-indigo-600">
+              y aparecerán en este lugar
+            </span>
+          </p>
+        </>
+      )}
     </>
   );
 };
